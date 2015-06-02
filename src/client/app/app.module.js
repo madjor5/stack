15adf08e0cd2dee7a358911ab12702da02641883
Mads Jørgensen <mads@madsjorgensen.dk>
2015-06-02 21:30:54 +0200

    added blank app

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -2,11 +2,7 @@
     'use strict';
 
     angular.module('app', [
-        'app.core',
-        'app.widgets',
-        'app.admin',
-        'app.dashboard',
-        'app.layout'
+        'ngMaterial'
     ]);
 
 })();
