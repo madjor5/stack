dc5bf4ffbfc9e898d24d2904050fa35a0db46e9f
Mads Jørgensen <mads@madsjorgensen.dk>
2015-07-15 08:08:21 +0200

    made routing work

--- src/client/app/teams/teams.route.js
+++ src/client/app/teams/teams.route.js
@@ -5,10 +5,22 @@
     .config(['$stateProvider', '$urlRouterProvider', config]);
 
   function config($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('/teams', '/teams/list');
     $stateProvider
       .state('teams', {
         url: '/teams',
+        template: '<ui-view />'
+      })
+      .state('teams.list', {
+        url: '/list',
         templateUrl: 'app/teams/teams.html'
+      })
+      .state('teams.details', {
+        url: '/details',
+        templateUrl: 'app/teams/teams.details.html',
+        onEnter: function() {
+          console.log('test');
+        }
       });
   }
 }());
